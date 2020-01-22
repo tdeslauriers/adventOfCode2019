34bd1fa647b9f5e57d52bd941f96ac7df03c500e
Tom Des Lauriers <thomas.deslauriers@gmail.com>
2020-01-21 23:07:32 -0600

    day4, part 1 puzzle solved.

--- day4/main.go
+++ day4/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 )
 
@@ -8,7 +9,7 @@ import (
 // challenge input: 171309-643603
 func main() {
 
-	modUpNum(123456)
+	fmt.Println(countPwds(loadChallenge(171309, 643603)))
 }
 
 func gTeT(s0, s1 int) bool {
@@ -34,4 +35,55 @@ func cutUpNum(i int) []int {
 	}
 
 	return j
+}
+
+func notDecrease(i []int) bool {
+
+	for j := 0; j < len(i)-1; j++ {
+
+		if !gTeT(i[j+1], i[j]) {
+
+			return false
+		}
+	}
+
+	return true
+}
+
+func hasPair(i []int) bool {
+
+	for j := 0; j < len(i)-1; j++ {
+
+		if i[j] == i[j+1] {
+
+			return true
+		}
+	}
+
+	return false
+}
+
+func countPwds(i []int) int {
+
+	c := 0
+	for j := 0; j < len(i); j++ {
+
+		k := cutUpNum(i[j])
+		if notDecrease(k) && hasPair(k) {
+
+			c++
+		}
+	}
+
+	return c
+}
+
+func loadChallenge(begin, end int) []int {
+
+	a := make([]int, end-begin+1)
+	for i := range a {
+
+		a[i] = begin + i
+	}
+	return a
 }
--- day4/main_test.go
+++ day4/main_test.go
@@ -6,11 +6,12 @@ import (
 
 func Test_gTeT(t *testing.T) {
 
+	w := 3
 	x := 3
 	y := 4
 	z := 5
 
-	if gTeT(x, y) && !gTeT(z, y) {
+	if gTeT(x, y) && !gTeT(z, y) && !gTeT(w, x) {
 		t.Error("Greater-than equal-to check incorrect.")
 	}
 
@@ -37,10 +38,75 @@ func Test_cutUpNum(t *testing.T) {
 	}
 }
 
-func Test_NotDecrease(t *testing.T) {
+func Test_notDecrease(t *testing.T) {
 
 	// challenge example inputs.
-	// a := 111111 // meets these criteria (double 11, never decreases).
-	// b := 223450 // does not meet these criteria (decreasing pair of digits 50).
-	// c := 123789 // does not meet these criteria (no double).
+	a := 111111 // meets these criteria (double 11, never decreases).
+	b := 223450 // does not meet these criteria (decreasing pair of digits 50).
+	c := 123789 // does not meet these criteria (no double).
+
+	if !notDecrease(cutUpNum(a)) {
+		t.Error("Expected input of 111111 to yield true.")
+	}
+
+	if notDecrease(cutUpNum(b)) {
+		t.Error("Expected input of 223450 to yield false.")
+	}
+
+	if !notDecrease(cutUpNum(c)) {
+		t.Error("Expected input of 123789 to yield true.")
+	}
+}
+
+func Test_hasPair(t *testing.T) {
+
+	// challenge example inputs
+	a := 111111 // meets these criteria (double 11, never decreases).
+	b := 223450 // does not meet these criteria (decreasing pair of digits 50).
+	c := 123789 // does not meet these criteria (no double).
+
+	if !hasPair(cutUpNum(a)) {
+		t.Error("Expected input of 111111 to yield true.")
+	}
+
+	if !hasPair(cutUpNum(b)) {
+		t.Error("Expected input of 223450 to yield true.")
+	}
+
+	if hasPair(cutUpNum(c)) {
+		t.Error("Expected input of 123789 to yield false.")
+	}
+
+}
+
+func Test_countPwds(t *testing.T) {
+
+	a := []int{111111, 223450, 123789, 123799}
+	b := countPwds(a)
+
+	if b != 2 {
+		t.Error("Given the input ", a,
+			"\ncount of possible passwords should be 2: ",
+			"\npositions a[0]: ", a[0], "and a[3]: ", a[3])
+	}
+}
+
+func Test_loadChallenge(t *testing.T) {
+
+	b := 5
+	e := 15
+
+	a := loadChallenge(b, e)
+
+	if len(a) != 11 {
+		t.Error("Expected length of a to be 11, got ", len(a))
+	}
+
+	if a[0] != 5 || a[2] != 7 || a[10] != 15 {
+		t.Error("Challenge load failed: ",
+			"\nExpected a[0] = 5, got ", a[0],
+			"\nExpected a[2] = 7, got ", a[2],
+			"\nExpected a[11] = 15, got ", a[10])
+	}
+
 }
